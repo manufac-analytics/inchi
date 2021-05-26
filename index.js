b44781902e67cd0f6883738f52bdb809a9bae7ef
maneetgoyal <maneetgoyal14@gmail.com>
2021-05-26 17:09:28 +0000

    refactor: Export CheckINCHIKey

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var ffi = require('ffi-napi');
-
-var libm = ffi.Library('./libinchi.so.1.06.00', {
-    'GetStringLength': [ 'int', [ 'string' ] ],
+const ffi = require('ffi-napi');
+const { CheckINCHIKey } = ffi.Library('./libinchi.so.1.06.00', {
     'CheckINCHIKey': ['int', [ 'string' ]]
-});
+}); 
 
-console.log(libm.GetStringLength("ddddkhhkjhkjhkjhkjhkjhkj"));
-console.log(libm.CheckINCHIKey("ddddkhhkjhkjhkjhkjhkjhkj")); // Invalid length and format
-console.log(libm.CheckINCHIKey("ddddkhhkjhkjhkjhkjhkjhkj111")); // Valid length and Invalid format
-console.log(libm.CheckINCHIKey("VNWKTOKETHGBQD-UHFFFAOYSA-N")); // Valid length and valid format
+// More functions may be exported soon. PRs welcome.
+module.exports = {CheckINCHIKey}
+
+// Example usage
+console.log(CheckINCHIKey("ddddkhhkjhkjhkjhkjhkjhkj")); // Invalid length and format
+console.log(CheckINCHIKey("ddddkhhkjhkjhkjhkjhkjhkj111")); // Valid length and Invalid format
+console.log(CheckINCHIKey("VNWKTOKETHGBQD-UHFFFAOYSA-N")); // Valid length and valid format
