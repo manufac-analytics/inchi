2264af64120aefa91aa8b4221e15ba637851510f
maneetgoyal <maneetgoyal14@gmail.com>
2021-05-27 12:52:02 +0530

    Add CheckINCHI

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const ffi = require("ffi-napi");
 const { CheckINCHIKey } = ffi.Library("./libinchi.so.1.06.00", {
   CheckINCHIKey: ["int", ["string"]],
+  CheckINCHI: ["int", ["string", "int"]],
 });
 
 // More functions may be exported soon. PRs welcome.
@@ -10,3 +11,6 @@ module.exports = { CheckINCHIKey };
 console.log(CheckINCHIKey("ddddkhhkjhkjhkjhkjhkjhkj")); // Invalid length and format
 console.log(CheckINCHIKey("ddddkhhkjhkjhkjhkjhkjhkj111")); // Valid length and Invalid format
 console.log(CheckINCHIKey("VNWKTOKETHGBQD-UHFFFAOYSA-N")); // Valid length and valid format
+console.log(CheckINCHI("ddddkhhkjhkjhkjhkjhkjhkj")); // Invalid length and format
+console.log(CheckINCHI("ddddkhhkjhkjhkjhkjhkjhkj111")); // Valid length and Invalid format
+console.log(CheckINCHI("VNWKTOKETHGBQD-UHFFFAOYSA-N")); // Valid length and valid format
