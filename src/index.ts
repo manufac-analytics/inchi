a349bd75616fe019b5821ffb166e2f4388fbf88d
maneetgoyal <maneetgoyal14@gmail.com>
2021-05-28 15:40:12 +0000

    Use __dirname

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Library } from "ffi-napi";
+import { join } from "path";
 
 // Use `readelf -s libinchi.so.1.06.00` to dump the SO content to console
 // More functions may be exported soon. PRs welcome.
-export const INCHIAPI = Library("./libinchi.so.1.06.00", {
+export const INCHIAPI = Library(join(__dirname, "./libinchi.so.1.06.00"), {
   CheckINCHIKey: ["int", ["string"]],
   CheckINCHI: ["int", ["string", "int"]],
   GetStringLength: ["int", ["string"]],
