25b10fe5884aa16d074c235b7f9130af3bab9e4a
maneetgoyal <maneetgoyal14@gmail.com>
2021-05-28 15:20:20 +0000

    Use TypeScript

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
-const ffi = require("ffi-napi");
+import { Library } from "ffi-napi";
 
 // Use `readelf -s libinchi.so.1.06.00` to dump the SO content to console
-const INCHIAPI = ffi.Library("./libinchi.so.1.06.00", {
+// More functions may be exported soon. PRs welcome.
+export const INCHIAPI = Library("./libinchi.so.1.06.00", {
   CheckINCHIKey: ["int", ["string"]],
   CheckINCHI: ["int", ["string", "int"]],
   GetStringLength: ["int", ["string"]],
 });
-
-// More functions may be exported soon. PRs welcome.
-module.exports = INCHIAPI;
