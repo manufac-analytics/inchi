1f00e769db72c748440a58417e2b59e12947488f
maneetgoyal <maneetgoyal14@gmail.com>
2021-05-29 02:31:32 +0000

    feat: Add JSON RPC server

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { Server } from "jayson";
+import { INCHIAPI } from ".";
+
+// Observe how easily FFIs are integrated with the RPC server.
+// Appears to be a very compatible match.
+const server = new Server(INCHIAPI);
+server.http().listen(3000, undefined, undefined, () => {
+  console.log("JSON/RPC server started on port 3000.");
+});
