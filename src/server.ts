8e3e1ca7265c4032fb576c2ab7a8d02a0b4d7954
maneetgoyal <maneetgoyal14@gmail.com>
2021-05-29 03:45:41 +0000

    Minor refactor

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import { Server } from "jayson";
 import { INCHIAPI } from ".";
 
-// Observe how easily FFIs are integrated with the RPC server.
-// Appears to be a very compatible match.
+// TODO: Improve mapping
 const server = new Server(INCHIAPI);
 
 // Start the HTTP server
-server.http().listen(3000, undefined, undefined, () => {
+server.http().listen(3000, () => {
   console.log("JSON/RPC server started on port 3000.");
 });
