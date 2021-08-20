88e5fd9fd31784341fdb6b2b2a03de6f474cbe76
maneetgoyal <maneetgoyal14@gmail.com>
2021-08-20 06:23:40 +0000

    docs: Add comment

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   testMatch: ["**/tests/**/*.test.ts"],
-  maxWorkers: 4,
+  maxWorkers: 4, // Without this tests are failing in CI
   verbose: true
 };
