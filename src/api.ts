fcab78f5fd052b8a33f648d25fbfe2befe9fafc8
Qurban Ahmad <ahmad76qurban@gmail.com>
2021-08-24 08:05:54 +0530

    feat: added more wrapper functions

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,6 @@
 import { INCHIAPI } from "./ffis";
-import { inchi_InputINCHI, inchi_OutputStruct } from "./headers";
+import { inchi_InputINCHI, inchi_OutputStruct, inchi_OutputStructEx } from "./headers";
+import refNAPI from "ref-napi";
 
 type StatusReturnType1 = 0 | -1 | 1 | 2 | 3;
 type StatusReturnType2 = 0 | -1 | 1 | 2 | 3 | 4;
@@ -8,7 +9,6 @@ type StatusReturnType3 = 0 | -1 | -2 | 1 | 2 | 3 | 4 | 5;
 export function CheckINCHIKey(input: string): StatusReturnType1 {
   return INCHIAPI.CheckINCHIKey(input) as StatusReturnType1;
 }
-
 
 export function CheckINCHI(input: string, strict?: boolean): StatusReturnType2 {
   return INCHIAPI.CheckINCHI(input, strict === true ? 1 : 0) as StatusReturnType2;
@@ -72,8 +72,6 @@ export interface INCHIOutput {
   szLog: string;
 }
 
-
-
 export function GetStructFromINCHI(input: string, options: string[] = [""]): StatusReturnType3 {
   let inputInchiOptions: GetINCHIExOptions = {
     NEWPSOFF: false,
@@ -116,5 +114,95 @@ export function GetStructFromINCHI(input: string, options: string[] = [""]): Sta
   const inchiIn = new inchi_InputINCHI(inchiInputObj);
   const inchiOutStruct = new inchi_OutputStruct();
   const output = INCHIAPI.GetStructFromINCHI(inchiIn.ref(), inchiOutStruct.ref());
+  return output as StatusReturnType3;
+}
+
+export function GetStructFromINCHIEx(input: string, options: string[] = [""]): StatusReturnType3 {
+  let inputInchiOptions: GetINCHIExOptions = {
+    NEWPSOFF: false,
+    DoNotAddH: false,
+    SNon: false,
+    SRel: false,
+    SRac: false,
+    SUCF: false,
+    ChiralFlagON: false,
+    ChiralFlagOFF: false,
+    SUU: false,
+    SLUUD: false,
+    FixedH: false,
+    RecMet: false,
+    KET: false,
+    "15T": false,
+    AuxNone: false,
+    Wnumber: false,
+    Wmnumber: false,
+    NoWarnings: false,
+    OutputSDF: false,
+    WarnOnEmptyStructure: false,
+    SaveOpt: false,
+    LooseTSACheck: false,
+    Polymers: false,
+    Polymers105: false,
+    NoFrameShift: false,
+    FoldCRU: false,
+    NPZz: false,
+    SAtZZ: false,
+    LargeMolecules: false,
+  };
+  for (let opt of options) {
+    inputInchiOptions[opt as keyof GetINCHIExOptions] = true;
+  }
+  const optionString = generateOptionsString(inputInchiOptions);
+  const inchiInputObj = { szInChI: "", szOptions: "" };
+  inchiInputObj.szInChI = input;
+  inchiInputObj.szOptions = optionString;
+  const inchiIn = new inchi_InputINCHI(inchiInputObj);
+  const inchiOutStructEx = new inchi_OutputStructEx();
+  const output = INCHIAPI.GetStructFromINCHI(inchiIn.ref(), inchiOutStructEx.ref());
+  return output as StatusReturnType3;
+}
+
+export function GetStructFromStdINCHI(input: string, options: string[] = [""]): StatusReturnType3 {
+  let inputInchiOptions: GetINCHIExOptions = {
+    NEWPSOFF: false,
+    DoNotAddH: false,
+    SNon: false,
+    SRel: false,
+    SRac: false,
+    SUCF: false,
+    ChiralFlagON: false,
+    ChiralFlagOFF: false,
+    SUU: false,
+    SLUUD: false,
+    FixedH: false,
+    RecMet: false,
+    KET: false,
+    "15T": false,
+    AuxNone: false,
+    Wnumber: false,
+    Wmnumber: false,
+    NoWarnings: false,
+    OutputSDF: false,
+    WarnOnEmptyStructure: false,
+    SaveOpt: false,
+    LooseTSACheck: false,
+    Polymers: false,
+    Polymers105: false,
+    NoFrameShift: false,
+    FoldCRU: false,
+    NPZz: false,
+    SAtZZ: false,
+    LargeMolecules: false,
+  };
+  for (let opt of options) {
+    inputInchiOptions[opt as keyof GetINCHIExOptions] = true;
+  }
+  const optionString = generateOptionsString(inputInchiOptions);
+  const inchiInputObj = { szInChI: "", szOptions: "" };
+  inchiInputObj.szInChI = input;
+  inchiInputObj.szOptions = optionString;
+  const inchiIn = new inchi_InputINCHI(inchiInputObj);
+  const inchiOutStruct = new inchi_OutputStruct();
+  const output = INCHIAPI.GetStructFromStdINCHI(inchiIn.ref(), inchiOutStruct.ref());
   return output as StatusReturnType3;
 }
