b5ba0e1db637843c5783db353bc198479e46e407
Qurban Ahmad <ahmad76qurban@gmail.com>
2021-08-24 21:15:45 +0530

    Port Inchi Functions Phase 2

--- src/api.ts
+++ src/api.ts
@@ -1,19 +1,58 @@
+// @ts-nocheck There are some issues in the Definitely Typed packages of the "ref" related dependencies.
 import { INCHIAPI } from "./ffis";
+import { inchi_OutputStruct, inchi_OutputStructEx } from "./headers";
 
-export function CheckINCHIKey(input: string): 0 | -1 | 1 | 2 | 3 {
-  return INCHIAPI.CheckINCHIKey(input) as 0 | -1 | 1 | 2 | 3;
-}
+// #region Types and Interfaces
 
-export function CheckINCHI(input: string, strict?: boolean): 0 | -1 | 1 | 2 | 3 | 4 {
-  return INCHIAPI.CheckINCHI(input, strict === true ? 1 : 0) as 0 | -1 | 1 | 2 | 3 | 4;
-}
+export type StatusReturnType1 = -1 | 0 | 1 | 2 | 3;
+export type StatusReturnType2 = -1 | 0 | 1 | 2 | 3 | 4;
 
-export function GetStringLength(input: string): number {
-  return INCHIAPI.GetStringLength(input);
-}
+export const GetINCHIReturnCode = {
+  /**
+   * Success; no errors or warnings
+   */
+  inchi_Ret_OKAY: 0,
+  /**
+   * Success; warning(s) issued
+   */
+  inchi_Ret_WARNING: 1,
+  /**
+   * Error: no InChI has been created
+   */
+  inchi_Ret_ERROR: 2,
+  /**
+   * Severe error: no InChI has been created (typically, memory allocation failure)
+   */
+  inchi_Ret_FATAL: 3,
+  /**
+   * Unknown program error
+   */
+  inchi_Ret_UNKNOWN: 4,
+  /**
+   * Previous call to InChI has not returned yet
+   */
+  inchi_Ret_BUSY: 5,
+  /**
+   * No structural data have been provided
+   */
+  inchi_Ret_EOF: -1,
+  /**
+   * Not used in InChI library
+   */
+  inchi_Ret_SKIP: -2
+} as const;
+export type GetINCHIReturnCode = typeof GetINCHIReturnCode[keyof typeof GetINCHIReturnCode];
 
 export interface GetINCHIOptions {
+  /**
+   * Meaning: Both ends of wedge point to stereocenters.
+   * Default behavior: Only narrow end of wedge points to stereocenter
+   */
   NEWPSOFF?: boolean;
+  /**
+   * Meaning: All hydrogens in input structure are explicit
+   * Default behavior: Add H according to usual valences
+   */
   DoNotAddH?: boolean;
   SNon?: boolean;
   SRel?: boolean;
@@ -47,7 +86,18 @@ export interface GetINCHIExOptions extends GetINCHIOptions {
   LargeMolecules?: boolean;
 }
 
-export function generateOptionsString(input: GetINCHIOptions | GetINCHIExOptions): string {
+export interface INCHIOutput {
+  szInChI: string;
+  szAuxInfo: string;
+  szMessage: string;
+  szLog: string;
+}
+
+// #endregion
+
+// #region Private Functions
+
+function generateOptionsString(input: GetINCHIOptions | GetINCHIExOptions): string {
   const marker = process.platform === "win32" ? "/" : "-";
   let options = Object.keys(input)
     .filter((ele) => {
@@ -59,9 +109,41 @@ export function generateOptionsString(input: GetINCHIOptions | GetINCHIExOptions
   return options.join(" ");
 }
 
-export interface INCHIOutput {
-  szInChI: string;
-  szAuxInfo: string;
-  szMessage: string;
-  szLog: string;
+// #endregion
+
+// #region Public Functions
+
+export function CheckINCHIKey(input: string): StatusReturnType1 {
+  return INCHIAPI.CheckINCHIKey(input) as StatusReturnType1;
 }
+
+export function CheckINCHI(input: string, strict?: boolean): StatusReturnType2 {
+  return INCHIAPI.CheckINCHI(input, strict === true ? 1 : 0) as StatusReturnType2;
+}
+
+export function GetStringLength(input: string): number {
+  return INCHIAPI.GetStringLength(input);
+}
+
+export function GetStructFromINCHI(input: string, options: GetINCHIOptions): GetINCHIReturnCode {
+  const inchiIn = { szInChI: input, szOptions: generateOptionsString(options) };
+  const inchiOutStruct = new inchi_OutputStruct();
+  const output = INCHIAPI.GetStructFromINCHI(inchiIn.ref(), inchiOutStruct.ref());
+  return output as StatusReturnType3;
+}
+
+export function GetStructFromINCHIEx(input: string, options: GetINCHIExOptions): GetINCHIReturnCode {
+  const inchiIn = { szInChI: input, szOptions: generateOptionsString(options) };
+  const inchiOutStructEx = new inchi_OutputStructEx();
+  const output = INCHIAPI.GetStructFromINCHIEx(inchiIn.ref(), inchiOutStructEx.ref());
+  return output as StatusReturnType3;
+}
+
+export function GetStructFromStdINCHI(input: string, options: GetINCHIOptions): GetINCHIReturnCode {
+  const inchiIn = { szInChI: input, szOptions: generateOptionsString(options) };
+  const inchiOutStruct = new inchi_OutputStruct();
+  const output = INCHIAPI.GetStructFromStdINCHI(inchiIn.ref(), inchiOutStruct.ref());
+  return output as StatusReturnType3;
+}
+
+// #endregion
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,1 @@
 export * from "./api";
-export * from "./ffis";
-export * from "./headers";
