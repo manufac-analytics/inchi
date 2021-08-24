b1bfefb51fb9bbb69adc8fc06db60eae87a88bfd
maneetgoyal <maneetgoyal14@gmail.com>
2021-08-24 07:45:07 +0000

    feat: Add GetINCHIReturnCode

--- src/api.ts
+++ src/api.ts
@@ -6,7 +6,42 @@ import { inchi_OutputStruct, inchi_OutputStructEx } from "./headers";
 
 export type StatusReturnType1 = -1 | 0 | 1 | 2 | 3;
 export type StatusReturnType2 = -1 | 0 | 1 | 2 | 3 | 4;
-export type StatusReturnType3 = -2 | -1 | 0 | 1 | 2 | 3 | 4 | 5;
+
+export const GetINCHIReturnCode = {
+  /**
+   * Success; no errors or warnings
+   */
+  inchi_Ret_OKAY: 0,
+  /**
+   * Success; warning(s) issued
+   */
+  inchi_Ret_WARNING: 1,
+  /**
+   * Error: no InChI has been created
+   */
+  inchi_Ret_ERROR: 2,
+  /**
+   * Severe error: no InChI has been created (typically, memory allocation failure)
+   */
+  inchi_Ret_FATAL: 3,
+  /**
+   * Unknown program error
+   */
+  inchi_Ret_UNKNOWN: 4,
+  /**
+   * Previous call to InChI has not returned yet
+   */
+  inchi_Ret_BUSY: 5,
+  /**
+   * No structural data have been provided
+   */
+  inchi_Ret_EOF: -1,
+  /**
+   * Not used in InChI library
+   */
+  inchi_Ret_SKIP: -2
+} as const;
+export type GetINCHIReturnCode = typeof GetINCHIReturnCode[keyof typeof GetINCHIReturnCode];
 
 export interface GetINCHIOptions {
   /**
@@ -90,21 +125,21 @@ export function GetStringLength(input: string): number {
   return INCHIAPI.GetStringLength(input);
 }
 
-export function GetStructFromINCHI(input: string, options: GetINCHIOptions): StatusReturnType3 {
+export function GetStructFromINCHI(input: string, options: GetINCHIOptions): GetINCHIReturnCode {
   const inchiIn = { szInChI: input, szOptions: generateOptionsString(options) };
   const inchiOutStruct = new inchi_OutputStruct();
   const output = INCHIAPI.GetStructFromINCHI(inchiIn.ref(), inchiOutStruct.ref());
   return output as StatusReturnType3;
 }
 
-export function GetStructFromINCHIEx(input: string, options: GetINCHIExOptions): StatusReturnType3 {
+export function GetStructFromINCHIEx(input: string, options: GetINCHIExOptions): GetINCHIReturnCode {
   const inchiIn = { szInChI: input, szOptions: generateOptionsString(options) };
   const inchiOutStructEx = new inchi_OutputStructEx();
   const output = INCHIAPI.GetStructFromINCHIEx(inchiIn.ref(), inchiOutStructEx.ref());
   return output as StatusReturnType3;
 }
 
-export function GetStructFromStdINCHI(input: string, options: GetINCHIOptions): StatusReturnType3 {
+export function GetStructFromStdINCHI(input: string, options: GetINCHIOptions): GetINCHIReturnCode {
   const inchiIn = { szInChI: input, szOptions: generateOptionsString(options) };
   const inchiOutStruct = new inchi_OutputStruct();
   const output = INCHIAPI.GetStructFromStdINCHI(inchiIn.ref(), inchiOutStruct.ref());
