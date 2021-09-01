97678b1551b2e60dc8b61daa756d653b2e8bebe0
Qurban Ahmad <ahmad76qurban@gmail.com>
2021-09-01 18:32:55 +0530

    feat: serve api functions

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,68 @@
 import { Server } from "jayson";
-import { INCHIAPI } from "./ffis";
+import {
+  GetStringLength,
+  GetStructFromINCHI,
+  GetStructFromINCHIOutput,
+  GetINCHIOptions,
+  GetStructFromINCHIEx,
+  GetStructFromINCHIExOutput,
+  GetStructFromStdINCHI,
+  CheckINCHIKey,
+  CheckINCHIKeyReturnCode,
+  CheckINCHI,
+  CheckINCHIReturnCode,
+} from "./api";
 
 const server = new Server({
   GetStringLength: (args: [string], callback: Function) => {
     let output = NaN;
     try {
-      output = INCHIAPI.GetStringLength(...args);
+      output = GetStringLength(...args);
+      callback(null, output);
+    } catch ({ message, name }) {
+      callback({ message, code: name === "TypeError" ? -32602 : 404 });
+    }
+  },
+  CheckINCHIKey: (args: [string], callback: Function) => {
+    let output: CheckINCHIKeyReturnCode;
+    try {
+      output = CheckINCHIKey(...args);
+      callback(null, output);
+    } catch ({ message, name }) {
+      callback({ message, code: name === "TypeError" ? -32602 : 404 });
+    }
+  },
+  CheckINCHI: (args: [string, boolean?], callback: Function) => {
+    let output: CheckINCHIReturnCode;
+    try {
+      output = CheckINCHI(...args);
+      callback(null, output);
+    } catch ({ message, name }) {
+      callback({ message, code: name === "TypeError" ? -32602 : 404 });
+    }
+  },
+  GetStructFromINCHI: (args: [string, GetINCHIOptions?], callback: Function) => {
+    let output: GetStructFromINCHIOutput;
+    try {
+      output = GetStructFromINCHI(...args);
+      callback(null, output);
+    } catch ({ message, name }) {
+      callback({ message, code: name === "TypeError" ? -32602 : 404 });
+    }
+  },
+  GetStructFromINCHIEx: (args: [string, GetINCHIOptions?], callback: Function) => {
+    let output: GetStructFromINCHIExOutput;
+    try {
+      output = GetStructFromINCHIEx(...args);
+      callback(null, output);
+    } catch ({ message, name }) {
+      callback({ message, code: name === "TypeError" ? -32602 : 404 });
+    }
+  },
+  GetStructFromStdINCHI: (args: [string, GetINCHIOptions?], callback: Function) => {
+    let output: GetStructFromINCHIOutput;
+    try {
+      output = GetStructFromStdINCHI(...args);
       callback(null, output);
     } catch ({ message, name }) {
       callback({ message, code: name === "TypeError" ? -32602 : 404 });
