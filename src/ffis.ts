a7c63261ffcebd86decf5047a1c888de4c628873
Qurban Ahmad <ahmad76qurban@gmail.com>
2021-09-03 16:25:00 +0530

    refactor: minor refactor

--- src/ffis.ts
+++ src/ffis.ts
@@ -2,6 +2,7 @@ import { Library } from "ffi-napi";
 import { join } from "path";
 import refNAPI from "ref-napi";
 import {
+  InchiInpData,
   inchi_Input,
   inchi_InputEx,
   inchi_InputINCHI,
@@ -42,17 +43,15 @@ export const INCHIAPI = Library(join(__dirname, "./libinchi.so.1.06.00"), {
   GetINCHIEx: [refNAPI.types.int, [refNAPI.refType(inchi_InputEx), refNAPI.refType(inchi_Output)]],
   FreeINCHI: [refNAPI.types.void, [refNAPI.refType(inchi_Output)]],
   Free_inchi_Input: [refNAPI.types.void, [refNAPI.refType(inchi_Input)]],
-  // please review the last parmater , it is written as inchi_InputData in inchi ref book but in description it is written as inchi_Input
   Get_inchi_Input_FromAuxInfo: [
     refNAPI.types.int,
-    [refNAPI.types.CString, refNAPI.types.int, refNAPI.types.int, refNAPI.refType(inchi_Input)],
+    [refNAPI.types.CString, refNAPI.types.int, refNAPI.types.int, refNAPI.refType(InchiInpData)],
   ],
   GetStdINCHI: [refNAPI.types.int, [refNAPI.refType(inchi_Input), refNAPI.refType(inchi_Output)]],
   FreeStdINCHI: [refNAPI.types.void, [refNAPI.refType(inchi_Output)]],
   Free_std_inchi_Input: [refNAPI.types.void, [refNAPI.refType(inchi_Input)]],
-  //same with below std function
   Get_std_inchi_Input_FromAuxInfo: [
     refNAPI.types.int,
-    [refNAPI.types.CString, refNAPI.types.int, refNAPI.refType(inchi_Input)],
+    [refNAPI.types.CString, refNAPI.types.int, refNAPI.refType(InchiInpData)],
   ],
 });
